0afcf0c9fc736654c77a65ae5bb6eb8f0d1cdc9b
emillycaaroline <emillyjchagas@gmail.com>
2024-03-22 18:45:20 -0300

    Parte funcional do codigo pronta

--- Pagina.php
+++ Pagina.php
@@ -1,42 +1,61 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My business</title>
+    <title>Document</title>
     <style>
-        body{
+         body{
             text-align: center;
-        }
+         }
     </style>
 </head>
 <body>
-    
-<section>
-        <span>Sistema de tabuada</span>
+    <p>NMR</p>
         <div>
             <form action="" method="post">
                 <input type="number" name="numero">
+
+                <p>mes</p>
+                <form action="" method="post">
+                <input type="number" name="mes">
+
                 <br><br>
                 <input type="submit">
             </form>
         </div>
-        <br>
-        <!-- PHP -->
-        <div>
-            <?php
-                $num = filter_input(INPUT_POST, "numero");
-                if($num){
-                    for ($i=1; $i < 11; $i++) { 
-                        echo "$num x $i =" . ($num * $i). "<br>";
-                    }
-                }else{
-                    // echo "<script>alert('informe um numero  ');</script"; FORMA DE FAZERE EM JAVASCRIPT
-                    echo "<h1>informe um numero</h1>";
-                }
-            ?>
-        </div>
-    </section>
 
+        <?php
+        $num = filter_input(INPUT_POST, "numero");
+        $mes = filter_input(INPUT_POST, "mes");
+        $min = 1927.02;
+        $Msmn = 20000;
+        $Mmen = 80000;
+
+         if($num == $Msmn){
+            $bonus = $Msmn * 0.01;
+            $f = $min + $bonus;
+                echo "Bateu a meta $f";
+
+            }if($num > $Msmn){
+                $sob = $num - $Msmn ;
+                $bon = $sob * 0.05;
+                $fin = $min + $bon;
+                echo "exedeu a meta semanal $fin";  
+
+            }
+            
+            if($mes > $Mmen){
+                $sobrou = $mes - $Mmen ;
+                $bonu = $sobrou * 0.1;
+                $finish = $min + $bonu;
+                echo "exedeu a meta mensal $finish"; 
+
+                $finaly = $f + $fin + $finish;
+            }
+            echo "Salario final é $finaly"
+
+         
+        ?>
 </body>
 </html>
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Atividade_empresa
 Atividade avaliativa de Programação Web II
+
+
+// codigo pronto
+<p>NMR</p>
+        <div>
+            <form action="" method="post">
+                <input type="number" name="numero">
+                <br><br>
+                <input type="submit">
+            </form>
+        </div>
+
+        <?php
+        $num = filter_input(INPUT_POST, "numero");
+        $nmr = 20000;
+
+         if($num >= $nmr){
+            $bonus = 20000 * 0.01;
+                echo "bateu $bonus";
+            }else{
+                echo "nao bateu";   
+            }
+         
+        ?>
