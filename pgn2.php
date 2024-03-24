1867a6027d771c1cd22b4a97f36c01957dec1265
emillycaaroline <emillyjchagas@gmail.com>
2024-03-24 00:38:44 -0300

    .

--- pgn2.php
+++ pgn2.php
@@ -7,21 +7,87 @@
     <style>
         body{
     text-align: center;
+    background: rgb(238,220,255);
+    background: rgb(93,31,151);
+    background: radial-gradient(circle, rgba(93,31,151,1) 0%, rgba(130,52,208,1) 37%, rgba(131,59,204,1) 68%, rgba(99,48,149,1) 81%, rgba(57,49,62,1) 100%);
+    /* tamanho */
+    padding: 1px;
+    /* Arredondar a borda */
+    border-radius: 50px;
+    color: rgb(255, 255, 255);
+}
+input{
+    border-radius: 10px;
+    padding: 3px;
 }
 
-div{
-   border: 5px solid rgb(0, 0, 0);
-   padding: 1px;
-   border-radius: 50px;
+.glow-on-hover {
+    width: 130px;
+    height: 40px;
+    border: none;
+    outline: none;
+    color: #fff;
+    background: #111;
+    cursor: pointer;
+    position: relative;
+    z-index: 0;
+    border-radius: 10px;
 }
+
+.glow-on-hover:before {
+    content: '';
+    background: linear-gradient(45deg, #5200a0, rgba(238,220,255,1) 0%, rgba(208,162,255,1) 74%, rgba(203,119,255,1) 100%);
+    position: absolute;
+    top: -2px;
+    left:-2px;
+    background-size: 400%;
+    z-index: -1;
+    filter: blur(5px);
+    width: calc(100% + 4px);
+    height: calc(100% + 4px);
+    animation: glowing 20s linear infinite;
+    opacity: 100;
+    transition: opacity .3s ease-in-out;
+    border-radius: 10px;
+}
+
+.glow-on-hover:active {
+    color: #000
+}
+
+.glow-on-hover:active:after {
+    background: transparent;
+}
+.glow-on-hover:hover:before {
+    opacity: 1;
+}
+
+.glow-on-hover:after {
+    z-index: -1;
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: #111;
+    left: 0;
+    top: 0;
+    border-radius: 10px;
+}
+
+@keyframes glowing {
+    0% { background-position: 0 0; }
+    50% { background-position: 400% 0; }
+    100% { background-position: 0 0; }
+}
+
     </style>
 </head>
 <body>
     
-    
+    <!-- Adicionei uma caixas de texto para informar o nome do funcionario, valores da semana e do mes e o botão -->
         <div>
             <p>Funcionario</p>
-       <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+       <form method="post" action="">
                 <input type="text" name="name"> <br> <br>
 
                 <br>semana 1
@@ -45,12 +111,14 @@ div{
                 <input type="number" name="mes">
 
                 <br><br>
-                <input type="submit">
+                <button class="glow-on-hover" type="submit">Conferir</button>
+
             </form>
+           
         
 
         <?php
-        
+        // Comecei fazendo variaveis para as caixas de texto
         $fun = filter_input(INPUT_POST, "name");
 
         $sem1 = filter_input(INPUT_POST, "sem1");
@@ -59,13 +127,13 @@ div{
         $sem4 = filter_input(INPUT_POST, "sem4");
 
         $mes = filter_input(INPUT_POST, "mes");
-
+        // Variaveis para o valor que é confirmado no fim e as metas a serem batidas.
         $min = 1927.02;
         $Msmn = 20000;
         $Mmen = 80000;
 
            
-
+            //Comecei executar o comando, fiz o mesmo para os dias das quatros semanas do mês, se for maior  ou igual começa executar, se não cancela.
             if($sem1 >= $Msmn){
             $v = $Msmn * 0.01;
             
@@ -147,6 +215,7 @@ div{
                             echo "E NÃO exedeu a meta Valor Bônus:  $vaaaa<br>";
                         }
 
+                        // comando para o mes, que se for maior começa executar a soma caso contrário a porcentagem a receber é 0
                         if($mes > $Mmen){
                             $sobrou = $mes - $Mmen ;
                             $bonu = $sobrou * 0.1;
@@ -161,6 +230,7 @@ div{
                         
 
 
+                        //Variavel responsavel pelo valor do salario final onde soma o bonus recebido de cada semana e mes e soma com o salario minimo
                         $sf = $va + $vaa +  $vaaa + $vaaaa + $v + $vv +$vvv + $vvvv + $bonu + $min;
                         echo "<h1> Salário final $sf </h1>";
                         
