7081bc517cdfe573462692120273b467b80d9888
emillycaaroline <emillyjchagas@gmail.com>
2024-03-25 21:46:44 -0300

    Código finalizado

--- README.md
+++ README.md
@@ -1,249 +1,28 @@
-# Atividade_empresa
-Atividade avaliativa de Programação Web II
+
+# 🔍 Atividades-Calculo de sálario
+
+## 📋 descrição
+
+- Desenvolvido para a matéria de `programação web II` no `Colégio Marista Irmão Acácio`, o arquivo tem a função de calcular a porcentagem de bônus que um funcionário ganha ao bater a meta semanal e mensal de uma empresa esse bônus é somado com um salário mínimo de 1927,02, o valor de meta semanal está estabecido como 20.000 e a cada batimento o funcionário ganha 1% sobre o valor e 5% sobre oque exceder esse valor e a meta mensal como 80.000(quatro vezes os 20.00 da semana) .
+  Para o calculo foi utilizado um código em PHP, em HTML foi colocado as caixas de texto e com CSS a estilização como a cor de fundo, fonte e tamanho das letras e borda no em volda das caixas de texto
+
+## 🖇️ Etapas:
+Criei um repositório no github e comecei a programação do site usando HTML, adicionei caixas de texto onde dono da empresa irá colocar os valores obtidos pelos funcionários.   Usei o PHP para os cálculos do site, comecando com a porcentagem de cada bonus do valor vendido do funcionario,  exibindo os valores adquiridos e somando com o salario minimo.   Com o CSS fiz a estilização geral da página, modificando o plano de fundo, as fontes de letras ouve polimento das caixas de texto, deixando-as redondas, com o CSS também coloquei o botão interativo que pode calcular os dados que o usuário está buscando. 
 
 
-  <!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-        body{
-    text-align: center;
-    background: rgb(238,220,255);
-    background: rgb(93,31,151);
-    background: radial-gradient(circle, rgba(93,31,151,1) 0%, rgba(130,52,208,1) 37%, rgba(131,59,204,1) 68%, rgba(99,48,149,1) 81%, rgba(57,49,62,1) 100%);
-    /* tamanho */
-    padding: 1px;
-    /* Arredondar a borda */
-    border-radius: 50px;
-    color: rgb(255, 255, 255);
-}
-input{
-    border-radius: 10px;
-    padding: 3px;
-}
+### 🛠️ Tecnologias Utilizadas
 
-.glow-on-hover {
-    width: 130px;
-    height: 40px;
-    border: none;
-    outline: none;
-    color: #fff;
-    background: #111;
-    cursor: pointer;
-    position: relative;
-    z-index: 0;
-    border-radius: 10px;
-}
+- github
+- HTML5
+- PHP
+- CSS
 
-.glow-on-hover:before {
-    content: '';
-    background: linear-gradient(45deg, #5200a0, #351c44, #8000ff, rgba(238,220,255,1) , rgba(208,162,255,1) , rgba(203,119,255,1) );
-    position: absolute;
-    top: -2px;
-    left:-2px;
-    background-size: 400%;
-    z-index: -1;
-    filter: blur(5px);
-    width: calc(100% + 4px);
-    height: calc(100% + 4px);
-    animation: glowing 20s linear infinite;
-    opacity: 100;
-    transition: opacity .3s ease-in-out;
-    border-radius: 10px;
-}
+## 👀 Viaual do site
 
-.glow-on-hover:active {
-    color: #000
-}
+![img](img/fotopagina.png)
 
-.glow-on-hover:active:after {
-    background: transparent;
-}
-.glow-on-hover:hover:before {
-    opacity: 1;
-}
+## ✒️ Autor
 
-.glow-on-hover:after {
-    z-index: -1;
-    content: '';
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    background: #111;
-    left: 0;
-    top: 0;
-    border-radius: 10px;
-}
+[<img loading="lazy" src="https://avatars.githubusercontent.com/u/127847857?v=4" width=115><br><sub>Emilly Caroline </sub>](https://github.com/emillycaaroline)<br><br><br>
 
-@keyframes glowing {
-    0% { background-position: 0 0; }
-    50% { background-position: 400% 0; }
-    100% { background-position: 0 0; }
-}
-
-    </style>
-</head>
-<body>
-    
-    <!-- Adicionei uma caixas de texto para informar o nome do funcionario, valores da semana e do mes e o botão -->
-        <div>
-            <h3>Funcionario</h3>
-       <form method="post" action="">
-                <input type="text" name="name"> <br> <br>
-
-                <br>semana 1
-            <form action="" method="post">
-                <input type="number" name="sem1"> <br>
-
-                <br>semana 2
-                <form action="" method="post">
-                <input type="number" name="sem2"> <br>
-
-                <br>semana 3
-                <form action="" method="post">
-                <input type="number" name="sem3"><br>
-
-                <br>semana 4 
-                <form action="" method="post">
-                <input type="number" name="sem4"><br>
-
-                <br>mes
-                <form action="" method="post">
-                <input type="number" name="mes">
-
-                <br><br>
-                <button class="glow-on-hover" type="submit">Conferir</button> <br> <br> <br>
-
-            </form>
-           
-        
-
-        <?php
-        // Comecei fazendo variaveis para as caixas de texto
-        $fun = filter_input(INPUT_POST, "name");
-
-        $sem1 = filter_input(INPUT_POST, "sem1");
-        $sem2 = filter_input(INPUT_POST, "sem2");
-        $sem3 = filter_input(INPUT_POST, "sem3");
-        $sem4 = filter_input(INPUT_POST, "sem4");
-
-        $mes = filter_input(INPUT_POST, "mes");
-        // Variaveis para o valor que é confirmado no fim e as metas a serem batidas.
-        $min = 1927.02;
-        $Msmn = 20000;
-        $Mmen = 80000;
-
-           
-            //Comecei executar o comando, fiz o mesmo para os dias das quatros semanas do mês, se for maior  ou igual começa executar, se não cancela.
-            if($sem1 >= $Msmn){
-            $v = $Msmn * 0.01;
-            
-                echo "<h3>Na primeira semana $fun bateu a meta Valor Bônus:$v.   ";
-
-            }else{
-                $v = 0;
-                echo "Na primeira semana $fun NÃO bateu a meta Valor Bônus:$v.   ";
-            }
-            if($sem1 > $Msmn){
-                $sob = $sem1 - $Msmn ;
-                $va = $sob * 0.05;
-               
-                echo "E exedeu a meta semanal Valor bonus:$va. <br> "; 
-            }else{
-                $va = 0;
-                echo "E NÃO exedeu a meta Valor Bônus:$va <br>";
-            }
-
-
-            if($sem2 >= $Msmn){
-                $vv = $Msmn * 0.01;
-                
-                    echo "<h3>Na segunda semana $fun bateu a meta Valor Bônus:$vv.   ";
-    
-                }else{
-                    $vv = 0;
-                    echo "Na segunda semana $fun NÃO bateu a meta Valor Bônus:$vv.   ";
-                }
-                if($sem2 > $Msmn){
-                    $sob = $sem2 - $Msmn ;
-                    $vaa = $sob * 0.05;
-                   
-                    echo "E exedeu a meta semanal Valor bonus: $vaa <br> "; 
-                }else{
-                    $vaa = 0;
-                    echo "E NÃO exedeu a meta Valor Bônus: $vaa<br>";
-                }
-
-
-
-                if($sem3 >= $Msmn){
-                    $vvv = $Msmn * 0.01;
-                    
-                        echo "<h3>Na terceira semana $fun bateu a meta Valor Bônus:$vvv.   ";
-        
-                    }else{
-                        $vvv= 0;
-                        echo "Na terceira semana $fun NÃO bateu a meta Valor Bônus:$vvv.   ";
-                    }
-                    if($sem3 > $Msmn){
-                        $sob = $sem3 - $Msmn ;
-                        $vaaa = $sob * 0.05;
-                       
-                        echo "E exedeu a meta semanal Valor bonus: $vaaa <br> "; 
-                    }else{
-                        $vaaa = 0;
-                        echo "E NÃO exedeu a meta Valor Bônus: $vaaa<br>";
-                    }
-
-
-
-                    if($sem4 >= $Msmn){
-                        $vvvv = $Msmn * 0.01;
-                        
-                            echo "<h3>Na quarta semana $fun bateu a meta Valor Bônus:$vvvv.   ";
-            
-                        }else{
-                            $vvvv = 0;
-                            echo "Na quarta semana $fun NÃO bateu a meta Valor Bônus:$vvvv."   ;
-                        }
-                        if($sem4 > $Msmn){
-                            $sob = $sem4 - $Msmn ;
-                            $vaaaa = $sob * 0.05;
-                           
-                            echo "E exedeu a meta semanal Valor bonus: $vaaaa <br> "; 
-                        }else{
-                            $vaaaa = 0;
-                            echo "E NÃO exedeu a meta Valor Bônus:  $vaaaa<br>";
-                        }
-
-                        // comando para o mes, que se for maior começa executar a soma caso contrário a porcentagem a receber é 0
-                        if($mes > $Mmen){
-                            $sobrou = $mes - $Mmen ;
-                            $bonu = $sobrou * 0.1;
-                            echo "<h2> Parabens $fun, você exedeu a meta mensal. Valor Bônus:$bonu<h2>  <br>";
-                        }else{
-                            $bonu = 00;
-                            echo "Não exedeu a meta $bonu<br> </h3>";
-                        }
-
-                          
-                      
-                        
-
-
-                        //Variavel responsavel pelo valor do salario final onde soma o bonus recebido de cada semana e mes e soma com o salario minimo
-                        $sf = $va + $vaa +  $vaaa + $vaaaa + $v + $vv +$vvv + $vvvv + $bonu + $min;
-                        echo "<h1> Salário final $sf </h1>";
-                        
-            
-
-
-            
-           
-        ?>
-        </div>
-</body>
-</html>       
+[linkedin] (https://www.linkedin.com/in/emilly-caroline-129936290/recent-activity/all/)
--- pgn2.php
+++ pgn2.php
@@ -11,7 +11,6 @@
             background: rgb(238, 220, 255);
             background: rgb(213, 170, 255);
             background: radial-gradient(circle, rgba(213, 170, 255, 1) 11%, rgba(195, 148, 255, 1) 76%, rgba(190, 144, 248, 1) 77%, rgba(77, 49, 83, 1) 100%, rgba(148, 187, 233, 1) 100%);
-            /* tamanho */
             padding: 1px;
             /* Arredondar a borda */
             border-radius: 50px;
@@ -22,6 +21,48 @@
             border-radius: 50px;
         }
 
+        .caixa-texto {
+            display: inline-block;
+            width: 100px;
+            margin-right: 10px;
+        }
+
+        .Form {
+            border: 5px solid #000;
+            border-radius: 50px;
+            margin-top: 10px;
+            width: 700px;
+            border-style: double;
+            border-color: #19002f;
+
+
+        }
+
+        h1 {
+            color: #8700ff;
+            font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+            border: 2px solid #000;
+            border-radius: 30px;
+            border-style: dotted;
+
+
+
+        }
+
+        h4 {
+            color: red;
+        }
+
+        p {
+            font-family: Georgia, 'Times New Roman', Times, serif;
+        }
+
+        .title {
+            color: #5e29a1;
+            font-size: 30px;
+        }
+
+        /* botão----------------------------------------------------------------------------------------------------------------------- */
         html,
         body {
             margin: 0;
@@ -35,17 +76,9 @@
             margin-top: 10px;
         }
 
-        h1 {
-            color: #5e29a1;
-            font-family: 'Courier New', Courier, monospace;
-
-        }
-
-
-
         .glow-on-hover {
-            width: 220px;
-            height: 50px;
+            width: 100px;
+            height: 30px;
             border: none;
             outline: none;
             color: #fff;
@@ -96,26 +129,6 @@
             top: 0;
             border-radius: 10px;
         }
-
-        @keyframes glowing {
-            0% {
-                background-position: 0 0;
-            }
-
-            50% {
-                background-position: 400% 0;
-            }
-
-            100% {
-                background-position: 0 0;
-            }
-        }
-
-        .caixa-texto {
-            display: inline-block;
-            width: 100px;
-            margin-right: 10px;
-        }
     </style>
 </head>
 
@@ -125,149 +138,154 @@
 
     <!-- Adicionei uma caixas de texto para informar o nome do funcionario, valores da semana e do mes e o botão -->
     <div>
-        <h1>Calculadora de Salário</h1>
-        <h2>Funcionario</h2>
-        <form method="post" action="">
-            <input type="text" name="name"> <br> <br>
+        <div class="Form">
+            <h2 class="title">Calculadora de Salário</h2>
+            <h2>Funcionario</h2>
+            <form method="post" action="">
+                <input type="text" name="name" required> <br> <br>
 
-            <br>semana 1
-            <form action="" method="post">
-                <input type="number" class="caixa-texto" name="sem1">
-
-                semana 2
+                <br>semana 1
                 <form action="" method="post">
-                    <input type="number" class="caixa-texto" name="sem2" required> <br><br>
+                    <input type="number" class="caixa-texto" name="sem1" required>
 
-                    semana 3
+                    semana 2
                     <form action="" method="post">
-                        <input type="number" class="caixa-texto" name="sem3">
+                        <input type="number" class="caixa-texto" name="sem2" required> <br><br>
 
-                        semana 4
+                        semana 3
                         <form action="" method="post">
-                            <input type="number" class="caixa-texto" name="sem4"> <br><br>
+                            <input type="number" class="caixa-texto" name="sem3" required>
 
-                            <br>mes
+                            semana 4
                             <form action="" method="post">
-                                <input type="number" name="mes">
+                                <input type="number" class="caixa-texto" name="sem4" required> <br>
 
-                                <br><br>
-                                <button class="glow-on-hover" type="submit">Conferir</button> <br> <br> 
+                                <br>mês
+                                <form action="" method="post">
+                                    <input type="number" name="mes" required>
 
-                            </form>
-
-
-
-                            <?php
-                            // Comecei fazendo variaveis para as caixas de texto
-                            $fun = filter_input(INPUT_POST, "name");
-
-                            $sem1 = filter_input(INPUT_POST, "sem1");
-                            $sem2 = filter_input(INPUT_POST, "sem2");
-                            $sem3 = filter_input(INPUT_POST, "sem3");
-                            $sem4 = filter_input(INPUT_POST, "sem4");
-
-                            $mes = filter_input(INPUT_POST, "mes");
-                            // Variaveis para o valor que é confirmado no fim e as metas a serem batidas.
-                            $min = 1927.02;
-                            $Msmn = 20000;
-                            $Mmen = 80000;
+                                    <br><br>
+                                    <button class="glow-on-hover" type="submit">Conferir</button> <br> <br>
 
 
-                            //Comecei executar o comando, fiz o mesmo para os dias das quatros semanas do mês, se for maior  ou igual começa executar, se não cancela.
-                            if ($sem1 >= $Msmn) {
-                                $v = $Msmn * 0.01;
-                            } else {
-                                $v = 0;
-                            }
-                            if ($sem1 > $Msmn) {
-                                $sob = $sem1 - $Msmn;
-                                $va = $sob * 0.05;
-                            } else {
-                                $va = 0;
-                            }
-                            //semana 2--------------------------------------------------------------------------
-
-                            if ($sem2 >= $Msmn) {
-                                $vv = $Msmn * 0.01;
-                            } else {
-                                $vv = 0;
-                            }
-                            if ($sem2 > $Msmn) {
-                                $sob = $sem2 - $Msmn;
-                                $vaa = $sob * 0.05;
-                            } else {
-                                $vaa = 0;
-                            }
+                                </form>
+        </div>
 
 
-                            //semana 3------------------------------------------------------------------------
-                            if ($sem3 >= $Msmn) {
-                                $vvv = $Msmn * 0.01;
-                            } else {
-                                $vvv = 0;
-                            }
-                            if ($sem3 > $Msmn) {
-                                $sob = $sem3 - $Msmn;
-                                $vaaa = $sob * 0.05;
-                            } else {
-                                $vaaa = 0;
-                            }
+        <?php
+        // Comecei fazendo variaveis para as caixas de texto
+        $fun = filter_input(INPUT_POST, "name");
 
+        $sem1 = filter_input(INPUT_POST, "sem1");
+        $sem2 = filter_input(INPUT_POST, "sem2");
+        $sem3 = filter_input(INPUT_POST, "sem3");
+        $sem4 = filter_input(INPUT_POST, "sem4");
 
-                            //semana 4---------------------------------------------------------------------------
-                            if ($sem4 >= $Msmn) {
-                                $vvvv = $Msmn * 0.01;
-                            } else {
-                                $vvvv = 0;
-                            }
-                            if ($sem4 > $Msmn) {
-                                $sob = $sem4 - $Msmn;
-                                $vaaaa = $sob * 0.05;
-                            } else {
-                                $vaaaa = 0;
-                            }
+        $mes = filter_input(INPUT_POST, "mes");
 
-                            // comando para o mes, que se for maior começa executar a soma caso contrário a porcentagem a receber é 0
-                            if ($mes > $Mmen) {
-                                $sobrou = $mes - $Mmen;
-                                $bonu = $sobrou * 0.1;;
-                            } else {
-                                $bonu = 00;
-                            }
+        // Variaveis para o valor que é confirmado no fim e as metas a serem batidas.
+        $min = 1927.02;
+        $Msmn = 20000;
+        $Mmen = 80000;
 
-                            $metaS = $v + $vv + $vvv + $vvvv;
-                            $exS = $va + $vaa + $vaaa + $vaaaa;
-
-                            echo "<h2>Resultados para $fun</h2>";
-
-                            echo "<h3>Bonus recebido por meta semanal: $metaS  ";
-
-                            echo "E exedeu a meta semanal Valor bonus:$va. <br> ";
-
-                            echo "<h3>Na segunda semana bateu a meta Valor Bônus:$exS.   ";
-                            echo "E exedeu a meta semanal Valor bonus: $vaa <br> ";
-
-                            echo "<h3>Na terceira semana bateu a meta Valor Bônus:$vvv.   ";
-                            echo "E exedeu a meta semanal Valor bonus: $vaaa <br> ";
-
-                            echo "<h3>Na quarta semana bateu a meta Valor Bônus:$vvvv.   ";
-                            echo "E exedeu a meta semanal Valor bonus: $vaaaa <br> ";
-
-                            echo "<h2>  exedeu a meta mensal. Valor Bônus:$bonu<h2>  <br>";
+        $totalSem = $sem1 + $sem2 + $sem3 + $sem4;
+        if ($mes != $totalSem) {
+            echo "<h4>Possivel Erro: A soma das semanas não é igual ao valor do mês.</h4>";
+            // Variavel para que o mês seja obrigatóriamente a soma das quatro semanas .
+        }
 
 
 
+        //Comecei executar o comando, fiz o mesmo para os dias das quatros semanas do mês, se for maior  ou igual começa executar, se não cancela.
+        if ($sem1 >= $Msmn) {
+            $v = $Msmn * 0.01;
+        } else {
+            $v = 0;
+        }
+        if ($sem1 > $Msmn) {
+            $sob = $sem1 - $Msmn;
+            $va = $sob * 0.05;
+        } else {
+            $va = 0;
+        }
+        //semana 2------------------------------------------------------------------------------------------------------------------
 
-                            //Variavel responsavel pelo valor do salario final onde soma o bonus recebido de cada semana e mes e soma com o salario minimo
-                            $sf = $va + $vaa +  $vaaa + $vaaaa + $v + $vv + $vvv + $vvvv + $bonu + $min;
-                            echo "<h1> Salário final $sf </h1>";
+        if ($sem2 >= $Msmn) {
+            $vv = $Msmn * 0.01;
+        } else {
+            $vv = 0;
+        }
+        if ($sem2 > $Msmn) {
+            $sob = $sem2 - $Msmn;
+            $vaa = $sob * 0.05;
+        } else {
+            $vaa = 0;
+        }
+
+
+        //semana 3---------------------------------------------------------------------------------------------------------------------
+        if ($sem3 >= $Msmn) {
+            $vvv = $Msmn * 0.01;
+        } else {
+            $vvv = 0;
+        }
+        if ($sem3 > $Msmn) {
+            $sob = $sem3 - $Msmn;
+            $vaaa = $sob * 0.05;
+        } else {
+            $vaaa = 0;
+        }
+
+
+        //semana 4------------------------------------------------------------------------------------------------------------------
+        if ($sem4 >= $Msmn) {
+            $vvvv = $Msmn * 0.01;
+        } else {
+            $vvvv = 0;
+        }
+        if ($sem4 > $Msmn) {
+            $sob = $sem4 - $Msmn;
+            $vaaaa = $sob * 0.05;
+        } else {
+            $vaaaa = 0;
+        }
+
+        // comando para o mes, que se for maior começa executar a soma caso contrário a porcentagem a receber é 0
+        if ($mes > $Mmen) {
+            $sobrou = $mes - $Mmen;
+            $bonu = $sobrou * 0.1;;
+        } else {
+            $bonu = 00;
+        }
+
+
+
+        $metaSemanal = $v + $vv + $vvv + $vvvv;
+        $excedentesemanal = $va + $vaa + $vaaa + $vaaaa;
+
+        echo "<h2>Resultado Salárial para $fun</h2>";
+
+        echo "<p>Salario minimo: $min</p>";
+
+        echo "<p>Bonus recebido por meta semanal: $metaSemanal</p>";
+
+        echo "<p>Bonus recebido por excedentes semanais $excedentesemanal.</p>";
+
+        echo "<p>  exedeu a meta mensal. Valor Bônus:$bonu</p>  ";
+
+
+
+
+        //Variavel responsavel pelo valor do salario final onde soma o bonus recebido de cada semana e mes e soma com o salario minimo
+        $sf = $va + $vaa +  $vaaa + $vaaaa + $v + $vv + $vvv + $vvvv + $bonu + $min;
+        echo "<h1> Salário final $sf </h1>";
 
 
 
 
 
 
-                            ?>
+        ?>
     </div>
 </body>
 
